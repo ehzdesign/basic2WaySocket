7d7b25638c463b09f100095c61912e550442f3ca
91.eahz@gmail.com <EHZ@Ericks-MacBook-Pro.local>
2016-10-13 16:21:37 -0400

    basic example to control two different index pages with socket.io 💻 🔁💻

---
+++ server.js
@@ -0,0 +1,34 @@
+//setup basic server
+
+var http	=	require('http');
+var path	=	require('path');
+var socketio =	require('socket.io');
+var express	=	require('express');
+var router	=	express();
+var server	=	http.createServer(router);
+var io =	socketio.listen(server);
+
+//	Use	router	to	point	requests	to	the	'client'	folder
+router.use(express.static(path.resolve(__dirname,	 'client')));
+
+
+//	Socket.io function	 that	runs	when	a	user	connects
+io.on('connection',	 function	(socket)	{
+  console.log('a	user	connected');
+
+  socket.on('button-clicked', function(msg){
+    io.emit('button-clicked', msg);
+  });
+  socket.on('button-click', function(msg){
+    io.emit('button-click', msg);
+  });
+  socket.on('disconnect',	 function	 ()	{
+    console.log('user	 disconnected');
+  });
+});
+
+//	Start	our	server
+server.listen(process.env.PORT ||	 3000,	process.env.IP ||	"0.0.0.0",	function	 ()	{
+  var addr =	server.address();
+  console.log("Our	server	is	listening	at",	addr.address +	":"	+	addr.port);
+});
