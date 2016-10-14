ac44083cf61f8be90d215d252fddc583c700ba6c
91.eahz@gmail.com <EHZ@Ericks-MacBook-Pro.local>
2016-10-13 22:07:42 -0400

    change to get method

--- client/master.js
+++ client/master.js
@@ -4,20 +4,24 @@ $(document).ready(function() {
 
 
 //get token from open triva db
-$.ajax({
-  url: 'https://opentdb.com/api_token.php?command=request'
-})
-.done(function(data) {
+//
+function getToken(){
+
+  $.ajax({
+    url: 'http://opentdb.com/api_token.php?command=request?handleData',
+    type: 'GET'
+  })
+
+};
+
+
+function handleData (data) {
+  // body...
   var token = data.token;
   var responseMessage = data.response_message;
 
   console.log("success");
   console.log('this is the token: ' + token);
   console.log('this is the responseMessage: ' + responseMessage);
-})
-.fail(function() {
-  console.log("error");
-})
-.always(function() {
-  console.log("complete");
-});
+
+}
